c14dbc797f67e67bac876b0387787b2a772ac252
Familia <Familia@Toshiba>
2018-08-04 09:03:08 -0500

    commit INICIAL

---
+++ src/main/java/com/bancodebogota/fdm/testci/TestCIMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebogota.fdm.testci;
+
+/**
+ *
+ * @author Familia
+ */
+public class TestCIMain {
+    public int sum(int n1, int n2){
+        return n1+n2;
+    }
+    public static void main(String[] args){
+        System.err.println("TESTCI");
+    }
+}
